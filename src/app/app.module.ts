4e2d21b9c9a21005ebc5c222111eeb2ad2e87df7
Alex Ramirez <e-jgiraldo@neoris.com>
2020-11-21 15:13:21 -0500

    Create routes and folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
 import { SparklineComponent } from './custom-charts/sparkline/sparkline.component';
 import { TimelineFilterBarChartComponent } from './custom-charts/timeline-filter-bar-chart/timeline-filter-bar-chart.component';
@@ -10,13 +9,18 @@ import { NgxChartsModule } from '@swimlane/ngx-charts/ngx-charts.module';
 import { NgxUIModule } from '@swimlane/ngx-ui';
 import { ComboChartComponent, ComboSeriesVerticalComponent } from './custom-charts/combo-chart';
 import { BubbleChartInteractiveModule } from './custom-charts/bubble-chart-interactive';
+import { DemoComponent } from './demo/demo.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'demo', component: DemoComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   providers: [
-    {
-      provide: APP_BASE_HREF,
-      useFactory: getBaseLocation
-    }
   ],
   imports: [
     NgxChartsModule,
@@ -24,14 +28,18 @@ import { BubbleChartInteractiveModule } from './custom-charts/bubble-chart-inter
     BrowserAnimationsModule,
     FormsModule,
     NgxUIModule,
-    BubbleChartInteractiveModule
+    BubbleChartInteractiveModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent,
     SparklineComponent,
     TimelineFilterBarChartComponent,
     ComboChartComponent,
-    ComboSeriesVerticalComponent
+    ComboSeriesVerticalComponent,
+    DemoComponent,
+    PricingComponent,
+    HomeComponent
   ],
   bootstrap: [AppComponent]
 })
